2c21a7530c45562b6b313c6f152e03dfdd743d4d
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-12-13 18:40:59 +0530

    Create common operations.java

---
+++ ArrayList Operations/common operations.java
@@ -0,0 +1,44 @@
+import java.util.*;  // 13/12/2025 
+
+public class Main{
+    public static void main(String a[]){
+        
+        ArrayList<Integer> nums = new ArrayList<Integer>();
+        nums.add(9);
+        nums.add(1);
+        nums.add(2);
+        nums.add(3);
+        nums.add(4);
+        nums.add(5);
+        nums.add(6);        // Add elements
+        nums.add(7);
+        nums.add(8);
+        
+        
+        System.out.println(nums);
+        
+        // System.out.println(nums.get(0)); // get element
+        
+        // nums.set(0, 0); // update element
+        
+        // nums.remove(0); // remove element
+        
+        // System.out.println(nums.size()); // size
+        
+        // System.out.println(nums.contains(9)); // is there, contains 
+        
+        // for(int i : nums){
+        //     System.out.println(i);  // Loop
+        // }
+        
+        // nums.clear(); // clear all elements
+        
+        // System.out.println(nums.isEmpty()); //checking array is empty or not 
+        
+        // Collections.sort(nums); //sort un-sorted elements
+        
+        System.out.println(nums);
+        
+        
+    }
+}
