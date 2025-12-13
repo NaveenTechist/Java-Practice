131ea4dedc37ab3488147dbf97517b2e0b72f2ad
N A V E E N <162016882+NaveenTechist@users.noreply.github.com>
2025-12-13 19:17:12 +0530

    Create Common operations.java

---
+++ LinkedList Operations/Common operations.java
@@ -0,0 +1,40 @@
+import java.util.LinkedList;
+
+public class Main{
+    public static void main(String a[]){
+        LinkedList<Integer> list = new LinkedList<>();
+        
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        
+         System.out.println(list);
+         
+        //  list.addFirst(9); // add at a first index like '0'
+        
+        // list.addLast(7); // Add last index 
+        
+        //  System.out.println(list.get(0));  // get element with index  
+         
+        //  list.set(0,100);     // add 100 at zero index 
+        
+        // list.remove(2);  // remove element with index
+        
+        // list.removeFirst(); // remove first means zero index
+        
+        // list.removeLast();  // remove last element from list
+        
+        //   System.out.println(list.size()); // size of the Array 
+        
+        //  System.out.println(list.contains(2)); //checking 2 is there, is there return true 
+        
+        // for(int i : list) System.out.println(i);   //itterate each element from array
+        
+        // list.clear(); // clear all elemets from array
+        
+        
+        //  System.out.println(list.isEmpty()); // checking array is empty or not 
+    }
+     
+}
